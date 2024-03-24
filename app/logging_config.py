dc251ca8a00d6dce165ca17f4f83992d1a90f4a3
Kaushik Puttaswamy <123877618+Kaushik-Puttaswamy@users.noreply.github.com>
2024-03-24 13:39:59 +0100

    Delete app directory

--- app/logging_config.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-def configure_logging():
-    # Configuring the logging module to display logs at the INFO level
-    logging.basicConfig(level=logging.INFO)
-
-# Calling the function to configure logging
-configure_logging()
