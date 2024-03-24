db8b7e319f8350585875cdbd80c92c23be57a274
Kaushik Puttaswamy <123877618+Kaushik-Puttaswamy@users.noreply.github.com>
2024-03-24 13:31:14 +0100

    Create logging_config.py

---
+++ app/logging_config.py
@@ -0,0 +1,8 @@
+import logging
+
+def configure_logging():
+    # Configuring the logging module to display logs at the INFO level
+    logging.basicConfig(level=logging.INFO)
+
+# Calling the function to configure logging
+configure_logging()
